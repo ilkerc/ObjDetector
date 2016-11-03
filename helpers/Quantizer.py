498c7032de04dc6d5b6fd80d01b3247fadb756cd
ilker cam <ilker.cam@isik.edu.tr>
2016-11-04 01:07:15 +0300

    Parameters options for layer

--- helpers/Quantizer.py
+++ helpers/Quantizer.py
@@ -31,7 +31,7 @@ class Quantizer(theano.Op):
     # TODO: Investigate Output Gradients,
     # TODO: If we decide to include ranges as learning parameters, hereby we need to define their gradients
     def grad(self, inputs, output_grads):
-        return [output_grads[0]]
+        return [output_grads[0], output_grads[1]]
 
 if __name__ == "__main__":
     theano.config.exception_verbosity = 'high'
