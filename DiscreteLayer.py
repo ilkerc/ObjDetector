48bcc0ea8f970c5aba3f5970b9e20eabfc478967
ilkerc <ilker.cam@isik.edu.tr>
2016-11-04 00:32:39 +0300

    Test updates

--- DiscreteLayer.py
+++ DiscreteLayer.py
@@ -1,13 +1,17 @@
 from lasagne.layers import Layer
 from helpers.DiscOP import DiscOP
+import theano.tensor as T
 
 
 class DiscreteLayer(Layer):
 
-    def __init__(self, incoming, mins, maxs, ranges, **kwargs):
+    def __init__(self, incoming, mins, maxs, ranges, quant, **kwargs):
         super(DiscreteLayer, self).__init__(incoming, **kwargs)
+        self.quant = self.add_param(quant, quant.shape, name='quant', trainable=False)
         self.op = DiscOP(mins=mins, maxs=maxs, ranges=ranges)
 
     def get_output_for(self, inputs, **kwargs):
         theta = inputs
-        return self.op(theta)
+	return self.quant * T.floor((theta/self.quant) + .5)
+        #return 0.12 * T.floor((theta / 0.12) + (1 / 2))
+        #return self.op(theta)
